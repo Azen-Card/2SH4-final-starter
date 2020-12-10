d703521a353e147964785ac4424a64839df7ae42
Azen-Card <70974923+Azen-Card@users.noreply.github.com>
2020-12-10 17:35:48 -0500

    Update Q1-CProgramming.c

--- Q1-CProgramming.c
+++ Q1-CProgramming.c
@@ -6,19 +6,18 @@ The question has four parts, answer all of them in this same file*/
  
 /*(a) [5 Marks] write structure Node that holds an integer value as well as a pointer to the next node*/
 //Answer:
-
-
-
-
+typedef structure{ 
+    int value;
+    structure node * next;
+} node;
 
 
 
 /*(b) [5 Marks] Write structure SLL that represents the singly linked list and holds a pointer to the first node in the list.*/
 //Answer:
-
-
-
-
+typedef structure{ 
+    node * head;
+} SLL;
 
 
 
@@ -28,9 +27,20 @@ returns:
 1 if the target was found in the list
 0, otherwise*/
 //Answer:
-
-
-
+int searchList(node* head, int target) 
+{
+    node* temp = head; 
+    int ans = 0; 
+    while (temp != NULL) 
+    {
+        if (temp->value == target) 
+        {
+            ans = 1; 
+        }
+        temp = temp->next; 
+    }
+    return ans;
+}
 
   
 /*(d) [10 Marks] test your function in main by creating a list of 10 nodes and then call the searchList function to search for a certain value, make sure you check 
@@ -39,18 +49,33 @@ the correctness of the returned value from the function by comparing it against 
 
 
 int main(){
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+	 node * head = NULL; 
+    head = (node *) malloc(sizeof(node));
+    node * temp = head; 
+    for (int i = 0; i < 10; i++) 
+    {
+        temp->value = i; 
+        if (i != 9) 
+        {
+            temp->next = (node *) malloc(sizeof(node));
+        }
+        temp = temp->next; 
+    }
+    node * temp1 = head; 
+    while(temp1 != NULL)
+    {
+        printf("%d, ", temp1->value); 
+        temp1 = temp1->next;
+    }
+    printf("\n");
+    int ans = searchList(head, 8);
+    printf("\n");
+    printf("searching for: %d, answer = %d\n", 8, ans); //check for 8
+    ans = searchList(head, 0);
+    printf("searching for: %d, answer = %d\n", 0, ans); //check for 0
+    ans = searchList(head, 10);
+    printf("searching for: %d, answer = %d\n", 10, ans); //check for 10    
+    ans = searchList(head, 9);
+    printf("searching for: %d, answer = %d\n", 9, ans); //check for 9
+    return 0;
 }
